580423d019942b4b2f1a1ddd6d61f4eb1eba9607
cclutta <chelsea.lutta@gmail.com>
2023-02-02 17:39:16 +0300

    Update

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Filtered Logger module
+"""
+
+def filter_datum(fields, redaction, message, separator):
+    """ That returns the log message obfuscated. """
+    for field in fields:
+        message = re.sub(fr'{field}=([^=]*){separator}',
+                         fr'{field}={redaction}{separator}', message)
+    return message
+
