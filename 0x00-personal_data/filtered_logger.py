b20c98b96903804671f8d4e620d6f242e0f220b1
cclutta <chelsea.lutta@gmail.com>
2023-02-02 18:14:54 +0300

    Update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,10 +2,15 @@
 """
 Filtered Logger module
 """
+import logging
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
     """ That returns the log message obfuscated. """
     for field in fields:
         message = re.sub(fr'{field}=([^=]*){separator}',
