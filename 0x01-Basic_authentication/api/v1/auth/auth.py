f76f624de6cf6eb637a36faff262735cb7e74ec9
cclutta <chelsea.lutta@gmail.com>
2023-02-06 22:25:48 +0300

    Update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,20 +11,20 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require auth method. """
 
-        
         if path and not path.endswith("/"):
             path += "/"
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
         if path is None:
-             return True
+            return True
         if path not in excluded_paths:
             return True
         return False
 
     def authorization_header(self, request=None) -> str:
         """ Authorization method. """
-        return None
+        if request is None:
+            return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Current user method. """
