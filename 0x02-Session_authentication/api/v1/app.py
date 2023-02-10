16ab6e5f88c0e38a60bad1b87c6c83740b97d9b8
cclutta <chelsea.lutta@gmail.com>
2023-02-10 16:37:50 +0300

    Update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,8 +54,10 @@ def before_request():
     if auth:
         if auth.require_auth(request.path, ['/api/v1/status/',
                                             '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
-            if not auth.authorization_header(request):
+                                            '/api/v1/forbidden/',
+                                            '/api/v1/auth_session/login/']):
+            if not auth.authorization_header(request) and\
+                 not auth.session_cookier(request):
                 abort(401)
             if not auth.current_user(request):
                 abort(403)
