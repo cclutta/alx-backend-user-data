161088649fbc03015aa5a7ea2f6d3f99b7068ed5
cclutta <chelsea.lutta@gmail.com>
2023-02-10 16:34:29 +0300

    Task 3

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -35,4 +36,10 @@ class Auth:
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Current user method. """
+        return None
+
+    def session_cookie(self, request=None):
+        """ returns a cookie value from a request. """
+        if request:
+            return request.cookies.get(os.getenv("SESSION_NAME"))
         return None
